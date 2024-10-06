28692607b9665b60645fbb23e5fc178bb989cb7c
adammessman <adammessman@gmail.com>
2024-10-05 21:15:35 -0500

    Functioning Random Path

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Pathfinding Starter Code
+
+I interpreted the random path the mean that the player should be able to visit a node it has visited before in order to avoid the player getting stuck in an infinite loop. I also interpreted this to mean that the player should be able to go back to the start node and back and forth between nodes to maintain a higher degree of randomness. The player should be able to visit the exit node, even without visiting the target node. 
--- pathing.py
+++ pathing.py
@@ -14,9 +14,31 @@ def set_current_graph_paths():
 def get_test_path():
     return graph_data.test_path[global_game_data.current_graph_index]
 
+# Gets a random node. Nodes that have not been visited will be given priority over visited nodes.  
+def get_random_node(current_graph, current_node_index):
+    node_options = current_graph[current_node_index][1]
+    random_node_index = random.randint(0, len(node_options))
+    return node_options[random_node_index]
+
 
 def get_random_path():
-    return [1,2]
+    current_graph = graph_data.graph_data[global_game_data.current_graph_index]
+    random_path_nodes = []
+    
+    target_node_index = global_game_data.target_node[global_game_data.current_graph_index]
+    exit_node_index = len(current_graph) - 1
+        
+    target_found = False
+    exit_found = False
+    
+    random_path_nodes.append(0)
+    current_node_index = 0
+    while ((target_node_index not in random_path_nodes) or (random_path_nodes[len(random_path_nodes) - 1] != exit_node_index)):
+        current_node_index = get_random_node(current_graph, current_node_index)
+        random_path_nodes.append(current_node_index)
+    
+    return random_path_nodes
+    
 
 
 def get_dfs_path():

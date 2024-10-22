eed8aaec94fea3cb7eb39762b04c6a5fc20b582f
adammessman <adammessman@gmail.com>
2024-10-22 15:10:38 -0500

    not quite BFS yet

--- pathing.py
+++ pathing.py
@@ -48,6 +48,7 @@ def get_random_path():
 
 
 def dfs_path_until_found(start_index, current_graph, target_index):
+    # Initializes the necessary structures
     path = []
     next_list = []
     next_list.append(start_index)
@@ -55,39 +56,87 @@ def dfs_path_until_found(start_index, current_graph, target_index):
     visited = set()
     parents = {}
 
+    # Loops until there is nothing in the stack or until a path is returned
     while next_list:
         current = next_list.pop(0)
         path.append(current)
         visited.add(current)
         adjacents = current_graph[current][1]
+        # Returns path is target node is added
         if (current == target_index):
             return path
+        # Traces back to parent if all of the adjacents are visited
         if all(element in visited for element in adjacents): 
-            # path.append(parents[current])
             next_list.insert(0,parents[current])
+        # Adds adjacents to the front of the stack if it has not been visited
         for i in range(len(adjacents)):
             adjacent = adjacents[i]
             if adjacent not in visited:
-                # visited.add(adjacent)
                 parents[adjacent] = current
                 next_list.insert(0, adjacent)
 
 def get_dfs_path():
 
+    # Sets travel nodes
     current_graph = graph_data.graph_data[global_game_data.current_graph_index]
     current_node_index = 0
     target_node_index = global_game_data.target_node[global_game_data.current_graph_index]
     exit_node_index = len(current_graph) - 1
     
+    # Calls helper function to set path
     path = dfs_path_until_found(current_node_index, current_graph, target_node_index)
     path = path + dfs_path_until_found(target_node_index, current_graph, exit_node_index)
 
     return path
 
+def bfs_path_until_found(start_index, current_graph, target_index):
+    # Initializes the necessary structures
+    path = []
+    next_list = []
+    next_list.append(start_index)
+
+    visited = set()
+    parents = {}
+
+    # Loops until there is nothing in the stack or until a path is returned
+    while next_list:
+        current = next_list.pop()
+        path.append(current)
+        visited.add(current)
+        adjacents = current_graph[current][1]
+        # Returns path is target node is added
+        if (current == target_index):
+            return path
+        # # Traces back to parent if all of the adjacents are visited
+        # if all(element in visited for element in adjacents): 
+        #     next_list.append(parents[current])
+        # Adds adjacents to the front of the stack if it has not been visited
+        for i in range(len(adjacents)):
+            adjacent = adjacents[i]
+            if adjacent not in visited:
+                parents[adjacent] = current
+                next_list.insert(0, adjacent)
+                if len(adjacents) > 1:
+                    next_list.insert(0, parents[adjacent])
+                if i == len(adjacents) - 1: 
+                    next_list.insert(0, adjacents[0])
+        
+
+
+
+
 
 
 def get_bfs_path():
-    return [1,2]
+
+    current_graph = graph_data.graph_data[global_game_data.current_graph_index]
+    current_node_index = 0
+    target_node_index = global_game_data.target_node[global_game_data.current_graph_index]
+    exit_node_index = len(current_graph) - 1
+
+    path = bfs_path_until_found(current_node_index, current_graph, target_node_index)
+
+    return path
 
 
 def get_dijkstra_path():

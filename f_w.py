2a2eaff6cba8ade214d9c5854e868cc6bc525645
adammessman <adammessman@gmail.com>
2024-11-28 22:56:39 -0600

    path function created
    I created the path function and added some of the parent functionality. None of it has been tested.

--- f_w.py
+++ f_w.py
@@ -19,6 +19,7 @@ def create_adj_matrix_from_list(graph):
     # matrix[a][b] = node a to node b, (row a, column b)
     # matrix[b][a] = node b to node a, (row b, column a)
     matrix = create_blank_matrix(graph)
+    parents = create_blank_matrix(graph)
 
     for index in range(len(graph)):
         node_coords = graph[index][0]
@@ -27,14 +28,28 @@ def create_adj_matrix_from_list(graph):
                 adj_node_coords = graph[adj_node][0]
                 edge_weight = euclidian_distance(node_coords, adj_node_coords)
                 matrix[index][adj_node] = edge_weight
+                
+                # TODO: CHECK THIS
+                parents[index][adj_node] = index
 
     return matrix
 
-def floyd_warshall_algorithm(graph_matrix):
+def floyd_warshall_algorithm(graph_matrix, parents):
      for k in range(len(graph_matrix)):
           for i in range(len(graph_matrix)):
                for j in range(len(graph_matrix)):
-                    graph_matrix[i][j] = min(graph_matrix[i][j], graph_matrix[i][k] + graph_matrix[k][j])
+                    if (graph_matrix[i][k] + graph_matrix[k][j] < graph_matrix[i][j]):
+                        graph_matrix[i][j] = graph_matrix[i][k] + graph_matrix[k][j]
+                        parents[i][j] = k
+                    
+                    # graph_matrix[i][j] = min(graph_matrix[i][j], graph_matrix[i][k] + graph_matrix[k][j])
+
+def floyd_warshall_path(parents, start_node, end_node):
+    path = []
+    parent = parents[start_node][end_node]
+    while parent is not None:
+        path.insert(0, parent)
+        parent = parents[start_node][parent]
 
 def main():
     current_graph = graph_data.graph_data[1]

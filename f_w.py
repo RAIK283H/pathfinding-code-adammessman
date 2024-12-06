1e01057b5ba0a5ab525f965e7892c0f148033090
adammessman <adammessman@gmail.com>
2024-12-05 21:50:31 -0600

    Comments / Cleaned up

--- f_w.py
+++ f_w.py
@@ -6,12 +6,14 @@ import sys
 def euclidian_distance(coord1, coord2):
     return math.sqrt((coord1[0] - coord2[0])**2 + (coord1[1] - coord2[1])**2)
 
+# Creates n x n matrix, where n is size of the graph
 def create_blank_matrix(graph):
     matrix = [sys.maxsize] * len(graph)
     for i in range(len(matrix)):
         matrix[i] = [sys.maxsize] * len(matrix)
     return matrix
 
+# Converts adjacency list to adjacency matrix
 def create_adj_matrix_from_list(graph):
     # Creates the inital matrix with -1 as default value signifying no path between nodes
     # In case of direction: 
@@ -30,7 +32,7 @@ def create_adj_matrix_from_list(graph):
 
     return matrix
 
-# NOTE: May need to return the matrix and parents   
+# Floyd-Warshall Algo
 def floyd_warshall_algorithm(graph_matrix, parents):
      for k in range(len(graph_matrix)):
           for i in range(len(graph_matrix)):
@@ -38,7 +40,8 @@ def floyd_warshall_algorithm(graph_matrix, parents):
                     if (graph_matrix[i][k] + graph_matrix[k][j] < graph_matrix[i][j]):
                         graph_matrix[i][j] = graph_matrix[i][k] + graph_matrix[k][j]
                         parents[i][j] = k
-                    
+
+# Reconstructs the paths              
 def floyd_warshall_path(parents, start_node, end_node):
     path = []
     parent = parents[start_node][end_node]
@@ -50,14 +53,17 @@ def floyd_warshall_path(parents, start_node, end_node):
     return path
 
 def main():
-    current_graph = graph_data.graph_data[8]
-    matrix = create_adj_matrix_from_list(current_graph)
-    parents = create_blank_matrix(current_graph)
-    print(parents)
-
-    floyd_warshall_algorithm(matrix, parents)
+    # Initializes needed data
+    current_graph = graph_data.graph_data[3]
     start_node_index = 0
     end_node_index = len(current_graph) - 1
+
+    # Initializes the needed matricies 
+    matrix = create_adj_matrix_from_list(current_graph)
+    parents = create_blank_matrix(current_graph)
+
+    # Runs Flyod-Warshall Algo
+    floyd_warshall_algorithm(matrix, parents)
     print(floyd_warshall_path(parents, start_node_index, end_node_index))
 
 main()
